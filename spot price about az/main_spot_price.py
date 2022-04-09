4fc768510074dc49c2f1bad8b647999751781a25
ASak1104 <khw56184@gmail.com>
2022-04-09 19:00:45 +0900

    fix bug that result is not store in buffers

--- spot price about az/main_spot_price.py
+++ spot price about az/main_spot_price.py
@@ -3,6 +3,7 @@ import time
 import boto3
 import pickle
 from ec2_package import *
+from multiprocessing import Manager
 from datetime import datetime, timedelta
 from pathos import multiprocessing as mp
 from dotenv import load_dotenv
@@ -25,10 +26,11 @@ if __name__ == '__main__':
     start_date = end_date - timedelta(minutes=10)
     result = dict()
 
-    buffers = [dict() for _ in range(len(regions))]
-    args = [(buffer, session, region, start_date, end_date) for region, buffer in zip(regions, buffers)]
+    manager = Manager()
+    buffers = manager.list()
+    args = [(buffers, session, region, start_date, end_date) for region in regions]
 
-    pool = mp.Pool(processes=min(len(buffers), mp.cpu_count() * 3))
+    pool = mp.Pool(processes=min(len(regions), mp.cpu_count() * 3))
     for region in pool.imap(store_spot_price, args):
         print(f'{region} end at {int(time.time() - start_time)}s')
     pool.close()
@@ -41,8 +43,6 @@ if __name__ == '__main__':
                     result[it][az] = value
             else:
                 result[it] = buffer[it]
-
-    print(f'end {int(time.time() - start_time)} s')
 
     with open(f"{end_date}.bin", 'wb') as file:
         pickle.dump(result, file)
