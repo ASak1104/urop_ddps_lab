66b43b164b640d875e9310e8242418db006d645e
ASak1104 <khw56184@gmail.com>
2022-04-09 17:29:33 +0900

    Add functions
    get_spot_price()
    store_spot_price()

--- ec2_package.py
+++ ec2_package.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+
 def get_regions(session, region='us-east-1'):
     client = session.client('ec2', region_name=region)
     describe_args = {
@@ -40,3 +44,40 @@ def get_it_az(session, region):
             break
         describe_args['NextToken'] = response['NextToken']
 
+
+def get_spot_price(session, region, start=None, end=None):
+    if type(end) is not datetime:
+        end = datetime.now()
+    if type(start) is not datetime:
+        start = end - relativedelta(months=1)
+    client = session.client('ec2', region)
+    describe_args = {
+        # 'AvailabilityZone': az,
+        # 'InstanceTypes': [it],
+        # 'ProductDescriptions': ['Linux/UNIX (Amazon VPC)'],
+        'MaxResults': 300,
+        'EndTime': end,
+        'StartTime': start
+    }
+    while True:
+        response = client.describe_spot_price_history(**describe_args)
+        for obj in response['SpotPriceHistory']:
+            az, it, os, price, timestamp = obj.values()
+            yield it, az, os, price, timestamp
+        if not response['NextToken']:
+            break
+        describe_args['NextToken'] = response['NextToken']
+
+
+def store_spot_price(params: tuple) -> str:
+    buffer, session, region, start, end = params
+    for it, az, os, price, timestamp in get_spot_price(session, region, start, end):
+        if it not in buffer:
+            buffer[it] = {az: {os: {'price': price, 'timestamp': timestamp}}}
+        elif az not in buffer[it]:
+            buffer[it][az] = {os: {'price': price, 'timestamp': timestamp}}
+        else:
+            if os in buffer[it][az] and timestamp < buffer[it][az][os]['timestamp']:
+                continue
+            buffer[it][az][os] = {'price': price, 'timestamp': timestamp}
+    return region
