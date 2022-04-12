3119c40248f17892c72c52c3821dd9d40931ebdf
ASak1104 <khw56184@gmail.com>
2022-04-12 12:39:37 +0900

    edit: whether print or not

--- spot price about az/test.py
+++ spot price about az/test.py
@@ -1,38 +1,46 @@
 import pickle
 
 
-def is_same_dict(d0: dict, d1: dict) -> bool:
+def is_same_dict(d0: dict, d1: dict, show=False) -> bool:
+    def print_ex(*args):
+        if show:
+            print(args[0])
+            for arg in args[1:]:
+                print('\t', arg)
+        return
+
+    result = True
     if type(d0) != type(d1):
-        print('type differ')
-        print(type(d0), type(d1))
-        return False
-    if len(d0) != len(d1):
-        print('length differ')
-        print(len(d0), len(d1))
-        return False
+        print_ex('type differ', type(d0), type(d1))
+        result = False
+    elif len(d0) != len(d1):
+        print_ex('length differ', len(d0), len(d1))
+        result = False
     for k0, v0 in d0.items():
         if k0 not in d1:
-            print(f'key [{k0}] is not in second dict')
-            return False
-        if type(v0) != type(d1[k0]):
-            print(f'in key [{k0}], type of value differ')
-            print(type(v0), type(d1[k0]))
-            return False
-        if type(v0) == dict:
-            if not is_same_dict(v0, d1[k0]):
-                return False
+            print_ex(f'key [{k0}] is not in second dict')
+            result = False
+        elif type(v0) != type(d1[k0]):
+            print_ex(f'in key [{k0}], type of value differ', type(v0), type(d1[k0]))
+            result = False
+        elif type(v0) == dict:
+            if not is_same_dict(v0, d1[k0], show=show):
+                result = False
         elif v0 != d1[k0]:
-            print(f'in key [{k0}], value differ')
-            print(v0, d1[k0])
-            return False
-    return True
+            print_ex(f'in key [{k0}], value differ', v0, d1[k0])
+            result = False
+    return result
 
 
-with open("2022-04-09 17:02:00.bin", 'rb') as file:
+file00 = 'base.bin'
+file01 = 'spot history/2022-04-12 02:48:06.bin'
+
+with open(file00, 'rb') as file:
     data01 = pickle.load(file)
 
-with open("2022-04-09 17:14:00.bin", 'rb') as file:
+with open(file01, 'rb') as file:
     data02 = pickle.load(file)
 
-print(is_same_dict(data01, data02))
+print(f'diff {file00} {file01}')
 print(data01 == data02)
+print(is_same_dict(data01, data02, show=True))
